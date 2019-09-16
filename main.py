adec66bdf1ce9a6916228dc588b7880086555b56
ksdfg <ksdfg123@gmail.com>
2019-09-16 13:40:50 +0530

    now with the start-stop button

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
-from time import sleep, time
+from threading import Thread
 
 import blynklib
 
 blynk = blynklib.Blynk("IwQyTY8WqD0SkkLROJM9mHomLPOrzRrj")
 
-heartrate = "2k"
+measuring = False
 
 
 @blynk.handle_event("write v1")
 def change_state(pin, value):
     print(pin, value)
-    if value[0] == '1':
-        for i in range(60):
-            blynk.virtual_write(0, i)
-            sleep(1)
+    global measuring
+    measuring = value[0] == '1'
 
 
+def measure():
+    while True:
+        if measuring:
+            '''
+            measure pulse rate here
+            '''
+            print("measuring")
+
+
+Thread(target=measure).start()
 while True:
     blynk.run()
