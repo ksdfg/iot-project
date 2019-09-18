1ce76201fd1a533d56c92d9718ccef0001b26429
ksdfg <ksdfg123@gmail.com>
2019-09-18 09:51:41 +0530

    added pulse measure function

--- pulse_rate.py
+++ pulse_rate.py
@@ -4,6 +4,7 @@ import time
 import Adafruit_ADS1x15
 
 adc = Adafruit_ADS1x15.ADS1015()
+
 # initialization 
 GAIN = 2 / 3
 curState = 0
@@ -43,34 +44,34 @@ def measure(func):
     global lastTime
 
     # read from the ADC
-    Signal = adc.read_adc(0, gain=GAIN)  # TODO: Select the correct ADC channel. I have selected A0 here
-    curTime = int(time.time() * 1000)
+    signal = adc.read_adc(0, gain=GAIN)  # TODO: Select the correct ADC channel. I have selected A0 here
+    curtime = int(time.time() * 1000)
 
-    sampleCounter += curTime - lastTime  # # keep track of the time in mS with this variable
-    lastTime = curTime
-    N = sampleCounter - lastBeatTime  # # monitor the time since the last beat to avoid noise
-    # print N, Signal, curTime, sampleCounter, lastBeatTime
+    sampleCounter += curtime - lastTime  # # keep track of the time in mS with this variable
+    lastTime = curtime
+    n = sampleCounter - lastBeatTime  # # monitor the time since the last beat to avoid noise
+    # print n, signal, curtime, sampleCounter, lastBeatTime
 
     # find the peak and trough of the pulse wave
-    if Signal < thresh and N > (IBI / 5.0) * 3.0:  # # avoid dichrotic noise by waiting 3/5 of last IBI
-        if Signal < T:  # T is the trough
-            T = Signal  # keep track of lowest point in pulse wave 
+    if signal < thresh and n > (IBI / 5.0) * 3.0:  # # avoid dichrotic noise by waiting 3/5 of last IBI
+        if signal < T:  # T is the trough
+            T = signal  # keep track of lowest point in pulse wave 
 
-    if Signal > thresh and Signal > P:  # thresh condition helps avoid noise
-        P = Signal  # P is the peak
+    if signal > thresh and signal > P:  # thresh condition helps avoid noise
+        P = signal  # P is the peak
         # keep track of highest point in pulse wave
 
     #  NOW IT'S TIME TO LOOK FOR THE HEART BEAT
     # signal surges up in value every time there is a pulse
-    if N > 250:  # avoid high frequency noise
-        if (Signal > thresh) and (Pulse == False) and (N > (IBI / 5.0) * 3.0):
+    if n > 250:  # avoid high frequency noise
+        if (signal > thresh) and not Pulse and (n > (IBI / 5.0) * 3.0):
             Pulse = True  # set the Pulse flag when we think there is a pulse
             IBI = sampleCounter - lastBeatTime  # measure time between beats in mS
             lastBeatTime = sampleCounter  # keep track of time for next pulse
 
             if secondBeat:  # if this is the second beat, if secondBeat == TRUE
                 secondBeat = False  # clear secondBeat flag
-                for i in range(0, 10):  # seed the running total to get a realisitic BPM at startup
+                for i in range(0, 10):  # seed the running total to get a realisitic bpm at startup
                     rate[i] = IBI
 
             if firstBeat:  # if it's the first time we found a beat, if firstBeat == TRUE
@@ -79,19 +80,19 @@ def measure(func):
                 return  # IBI value is unreliable so discard it
 
             # keep a running total of the last 10 IBI values
-            runningTotal = 0  # clear the runningTotal variable    
+            running_total = 0  # clear the running_total variable    
 
             for i in range(0, 9):  # shift data in the rate array
                 rate[i] = rate[i + 1]  # and drop the oldest IBI value 
-                runningTotal += rate[i]  # add up the 9 oldest IBI values
+                running_total += rate[i]  # add up the 9 oldest IBI values
 
             rate[9] = IBI  # add the latest IBI to the rate array
-            runningTotal += rate[9]  # add the latest IBI to runningTotal
-            runningTotal /= 10  # average the last 10 IBI values 
-            BPM = 60000 / runningTotal  # how many beats can fit into a minute? that's BPM!
-            func('BPM: {}'.format(BPM))
+            running_total += rate[9]  # add the latest IBI to running_total
+            running_total /= 10  # average the last 10 IBI values 
+            bpm = 60000 / running_total  # how many beats can fit into a minute? that's bpm!
+            func('bpm: {}'.format(bpm))
 
-    if Signal < thresh and Pulse == True:  # when the values are going down, the beat is over
+    if signal < thresh and Pulse == True:  # when the values are going down, the beat is over
         Pulse = False  # reset the Pulse flag so we can do it again
         amp = P - T  # get amplitude of the pulse wave
         thresh = amp / 2 + T  # set thresh at 50% of the amplitude
