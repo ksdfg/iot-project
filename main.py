424d2ce64ce746adb3df09f070bffb505d21a635
ksdfg <ksdfg123@gmail.com>
2019-09-18 09:39:07 +0530

    added pulse measure function

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from threading import Thread
+from time import sleep, time
 
 import blynklib
 
+import pulse_rate as pr
+
 blynk = blynklib.Blynk("IwQyTY8WqD0SkkLROJM9mHomLPOrzRrj")
 
-measuring = False
+measuring = False  # boolean to figure out if we are measuring right now or not
 
 
+# when the user clicks button
 @blynk.handle_event("write v1")
 def change_state(pin, value):
     print(pin, value)
@@ -14,15 +18,17 @@ def change_state(pin, value):
     measuring = value[0] == '1'
 
 
+# measuring the pulse rate
 def measure():
     while True:
-        if measuring:
-            '''
-            measure pulse rate here
-            '''
-            print("measuring")
+        if measuring:  # measure pulse rate here
+            pr.lastTime = int(time() * 1000)  # set to current time
+            pr.measure(lambda x: blynk.virtual_write(0, x))  # measure pulse and send to app
+            sleep(0.005)
+        else:
+            print('not measuring')
 
 
-Thread(target=measure).start()
+Thread(target=measure).start()  # start measuring in seperate thread
 while True:
-    blynk.run()
+    blynk.run()  # start polling for action from RPi
